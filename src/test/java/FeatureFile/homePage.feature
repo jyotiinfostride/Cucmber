ea4865cddbcb09b96e4ebc4925cbdb4a03ad8341
Jyoti <jyoti@infostride.com>
2022-02-10 19:40:14 +0530

    Coman commit

--- src/test/java/FeatureFile/homePage.feature
+++ src/test/java/FeatureFile/homePage.feature
@@ -1,14 +1,11 @@
-Feature: Checking Login functionality of the application
-  Scenario: Checking Sauce Labs Backpack Cart
-    Given user is on Login Page
-    Then user enters username in  username textfield
-    Then user enters password in password textfield
-    And click on the Login button
-    Then Click on Sauce Labs Backpack Add to Cart
+Feature: homePage
 
-  Scenario: Checking Sauce Labs Bike Light
-    Given user is on Login Page
-    Then user enters username in  username textfield
-    Then user enters password in password textfield
-    And click on the Login button
-    Then Click on Sauce Labs Bike Light Add to Cart
+  Scenario: Successful check the Cart of an inventory page
+    Given user is on inventory page
+    When Click on Sauce Labs Backpack Add to Cart
+
+
+
+
+
+
--- src/test/java/StepDefinations/HomePage.java
+++ src/test/java/StepDefinations/HomePage.java
@@ -1,30 +1,29 @@
 package StepDefinations;
 
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
-
 public class HomePage {
     WebDriver driver;
 
-    @Given("^user is on Login Page$")
-    public void user_is_on_Login_Page() {
+
+    @Given("^user is on inventory page$")
+    public void user_is_on_inventory_page() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
     }
-    @Then("Click on Sauce Labs Backpack Add to Cart")
+
+    @When("^Click on Sauce Labs Backpack Add to Cart$")
     public void click_on_Sauce_Labs_Backpack_Add_to_Cart() {
         driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
     }
-    @Then("Click on Sauce Labs Bike Light")
-    public void Click_on_Sauce_Labs_Bike_Light(){
-        driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
-    }
+
 }
